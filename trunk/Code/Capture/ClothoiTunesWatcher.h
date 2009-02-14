dd63d4ea6d2f71977477e038e8423966f1fd93d1
jnj <jnj@8f81d046-9653-11dd-946f-4bcfd69f6e33>
2009-02-14 17:02:10 +0000

    add itunes monitoring

--- trunk/Code/Capture/ClothoiTunesWatcher.h
+++ trunk/Code/Capture/ClothoiTunesWatcher.h
@@ -7,9 +7,9 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "ClothoLogger.h"
 
-
-@interface ClothoiTunesWatcher : NSObject {
+@interface ClothoiTunesWatcher : ClothoLogger {
 
 }
 
--- trunk/Code/Capture/ClothoiTunesWatcher.m
+++ trunk/Code/Capture/ClothoiTunesWatcher.m
@@ -10,5 +10,37 @@
 
 
 @implementation ClothoiTunesWatcher
+- (id) init {
+  self = [super init];
+  if (self != nil) {
+    [[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(trackChanged:) name:@"com.apple.iTunes.playerInfo" object:nil];
+  }
+  return self;
+}
 
+- (NSString *)logName {
+  return @"iTunes.log";
+}
+
+
+- (void)trackChanged:(NSNotification *)notif {
+  NSDictionary *info = [notif userInfo];
+  NSString *message = [NSString stringWithFormat:@"%@\t%@\t%@\t%@\t%@\n",
+                       [info valueForKey:@"Location"],
+                       [info valueForKey:@"Name"],
+                       [info valueForKey:@"Artist"],
+                       [info valueForKey:@"Album"],
+                       [info valueForKey:@"Player State"]];
+  
+  
+  fprintf(log, [message UTF8String]);
+  
+  fflush(log);
+  
+}
+
+- (void)dealloc {
+  [[NSDistributedNotificationCenter defaultCenter] removeObserver:self];
+  [super dealloc]; 
+}
 @end
