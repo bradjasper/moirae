c9053912fec07cb917be93c489f8a06c58c76cd2
JCadibuS <JCadibuS@8f81d046-9653-11dd-946f-4bcfd69f6e33>
2009-05-07 00:40:21 +0000

    Logging CPU usage and desktop size now. System snapshots are put into folders corresponding to the day.

--- trunk/Code/Capture/ClothoScreenWatcher.m
+++ trunk/Code/Capture/ClothoScreenWatcher.m
@@ -7,7 +7,9 @@
 //
 
 #import "ClothoScreenWatcher.h"
+#import <Foundation/NSZone.h>
 #include <ApplicationServices/ApplicationServices.h>
+
 
 @implementation ClothoScreenWatcher
 
@@ -61,9 +63,10 @@
 
 - (void)captureScreen_helper{
     
-	CGImageRef screenShot = CGWindowListCreateImage(CGRectInfinite, kCGWindowListOptionOnScreenOnly, kCGNullWindowID, kCGWindowImageDefault);
+	CGImageRef screenShot = CGWindowListCreateImage(CGRectInfinite, kCGWindowListOptionAll, kCGNullWindowID, kCGWindowImageDefault);
 	//[self writeCGImage:screenShot toFile:[path stringByAppendingPathExtension:@"png"]];
     [self logScreenShot:screenShot];
+
     CGImageRelease(screenShot);
 }
 
@@ -76,13 +79,16 @@
 
 - (void)captureSystemSnapshot_help{
   
-  NSMutableArray *list = (NSMutableArray *)CGWindowListCopyWindowInfo(kCGWindowListOptionOnScreenOnly |  
+  NSMutableArray *list = (NSMutableArray *)CGWindowListCopyWindowInfo(kCGWindowListOptionAll |  
                                                                       kCGWindowListExcludeDesktopElements, kCGNullWindowID);
   
   
-  list = CGWindowListCopyWindowInfo(kCGWindowListOptionOnScreenOnly, kCGNullWindowID);
+  list = CGWindowListCopyWindowInfo(kCGWindowListOptionAll, kCGNullWindowID);
   [self compareAppFolderToProcess:list];
-  
+    
+    [list addObject:[NSDictionary dictionaryWithObject:[self retrieveDesktopSize] forKey:@"DesktopSize"]];
+    [list addObject:[NSDictionary dictionaryWithObject:[self retrieveCPUusage] forKey:@"CPUUsage"]];
+    
   [self logSystemSnapshot:list];
   
   [list release];
@@ -93,9 +99,80 @@
 	
 	[self captureSystemSnapshot_help];
 	
-	[self performSelector:@selector(captureSystemSnapshot) withObject:nil afterDelay:300.0];
+	[self performSelector:@selector(captureSystemSnapshot) withObject:nil afterDelay:60.0];
 }
 
+- (NSArray *)retrieveCPUusage {
+    NSTask *task;
+    task = [[NSTask alloc] init];
+    [task setLaunchPath:@"/usr/bin/top"];
+    
+    // "-l3" is the number of times top is run
+    NSArray *arguments = [NSArray arrayWithObjects:@"-FR", @"-l3", nil];
+    [task setArguments:arguments];
+    
+    NSPipe *pipe = [NSPipe pipe];
+    [task setStandardOutput:pipe];
+    
+    NSFileHandle *file = [pipe fileHandleForReading];
+    
+    //  run terminal command: "top -FR -l3"
+    [task launch];
+    
+    NSData *data = [file readDataToEndOfFile];
+    
+    NSString *output = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSArray *outputComponents = [NSArray arrayWithArray:[output componentsSeparatedByString:@"Processes:"]];
+    NSMutableArray *CPUsamples = [[[NSMutableArray alloc] init] autorelease];
+    
+    for (id topOutput in outputComponents) {
+        NSRange usageRange = [topOutput rangeOfString:@"CPU usage:"];
+        if (usageRange.length != 0) {
+            usageRange.length += 38;
+            
+            NSString *justUsage = [topOutput substringWithRange:usageRange];
+            NSRange percentage;
+            percentage.length = 6;
+            
+            percentage.location = 10;
+            NSString *user = [justUsage substringWithRange:percentage];
+            percentage.location = 23;
+            NSString *sys = [justUsage substringWithRange:percentage];
+            percentage.location = 35;
+            NSString *idle = [justUsage substringWithRange:percentage];
+            
+            [CPUsamples addObject:[NSDictionary dictionaryWithObjectsAndKeys:
+                                   user, @"user",
+                                   sys, @"sys",
+                                   idle, @"idle", nil]];
+        }
+    }
+    return CPUsamples;
+}
+
+- (NSDictionary *)retrieveDesktopSize {
+    //  log each screen's size and the total size
+    NSArray *allScreens = [NSArray arrayWithArray:[NSScreen screens]];
+    NSEnumerator *screenEnum = [allScreens objectEnumerator];
+    NSMutableDictionary *screensAndSizes = [[[NSMutableDictionary alloc] init] autorelease];
+    CGFloat totalSum = 0;
+    int i;
+    for (i=0; i<[allScreens count]; i++) {
+        id screen = [screenEnum nextObject];
+        CGFloat height = CGRectGetHeight(NSRectToCGRect([screen frame]));
+        CGFloat width = CGRectGetWidth(NSRectToCGRect([screen frame]));
+        totalSum += height*width;
+        
+        NSString *screenWidth = [@"Width" stringByAppendingString:[NSString stringWithFormat:@"%d", (i+1)]];
+        NSString *screenHeight = [@"Height" stringByAppendingString:[NSString stringWithFormat:@"%d", (i+1)]];
+        
+        [screensAndSizes setValue:[NSNumber numberWithFloat:width] forKey:screenWidth];
+        [screensAndSizes setValue:[NSNumber numberWithFloat:height] forKey:screenHeight];
+    }
+    [screensAndSizes setValue:[NSNumber numberWithFloat:totalSum] forKey:@"TotalSize"];
+    
+    return screensAndSizes;
+}
 
 - (void)writeCGImage:(CGImageRef)image toFile:(NSString *)path {
 	
