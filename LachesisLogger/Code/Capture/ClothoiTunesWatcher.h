10f3cd18743fc5b02efe98733fd807f9a4ee73b0
JCadibuS <JCadibuS@8f81d046-9653-11dd-946f-4bcfd69f6e33>
2010-02-28 22:12:30 +0000

    putting a good logger

---
+++ LachesisLogger/Code/Capture/ClothoiTunesWatcher.h
@@ -0,0 +1,16 @@
+//
+//  ClothoiTunesWatcher.h
+//  Clotho
+//
+//  Created by Nicholas Jitkoff on 10/5/08.
+//  Copyright 2008 Blacktree. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "ClothoLogger.h"
+
+@interface ClothoiTunesWatcher : ClothoLogger {
+
+}
+
+@end
