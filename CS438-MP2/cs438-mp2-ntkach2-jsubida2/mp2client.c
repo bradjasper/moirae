ddfcb35f7c35919e72334ff8bd9064de793c14b7
JCadibuS <JCadibuS@8f81d046-9653-11dd-946f-4bcfd69f6e33>
2010-03-19 00:48:54 +0000

    adding .tar file

---
+++ CS438-MP2/cs438-mp2-ntkach2-jsubida2/mp2client.c
@@ -0,0 +1,191 @@
+/*
+ * client.c -- a datagram "client" demo
+ * Code is derived from Beej's Guide: 
+ * http://beej.us/guide/bgnet/output/html/multipage/clientserver.html#simpleserver
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#include "mp2.c"
+
+#define SERVERPORT "1357"    // the port users will be connecting to
+
+#define MAXBUFLEN 1000
+#define FRAMESIZE 100
+
+
+struct frameHeader
+{
+	uint16_t checksum;
+	uint16_t frameNum;
+	uint8_t slt;
+	char data[FRAMESIZE];
+};
+
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(int argc, char *argv[])
+{
+
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_storage their_addr;
+	size_t addr_len;
+	int rv;
+    int numbytes;
+	char buf[MAXBUFLEN];
+	char s[INET6_ADDRSTRLEN];
+	char * filename;
+	FILE * outputFile;
+	int filesize;
+	int i;
+	struct frameHeader frH;
+	
+	memset(&frH, 0, sizeof(frH));
+
+	// check number of arguments
+	if (argc < 3) {
+		fprintf(stderr,"usage: missing 2 arguments (hostname address and a message\n");
+		exit(1);
+	}
+	else if(argc > 3) {
+		fprintf(stderr,"usage: too many arguments\n");
+		exit(1);
+	}
+
+    // initialize variables
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
+
+    // retrieve address info for given IP address, port, hints, and server info
+    if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and make a socket
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("client: socket");
+            continue;
+        }
+
+        break;
+    }
+
+    // if addrinfo p is null, then unable to bind
+    if (p == NULL) {
+        fprintf(stderr, "client: failed to bind socket\n");
+        return 2;
+    }
+
+    // send message 
+    if ((numbytes = sendto(sockfd, argv[2], strlen(argv[2]), 0,
+             p->ai_addr, p->ai_addrlen)) == -1) {
+        perror("client: sendto");
+        exit(1);
+    }
+
+    printf("client: sent %d bytes to %s\n", numbytes, argv[1]);
+	
+	// wait to receive:
+    printf("client: waiting to recvfrom...\n");
+	
+	// 1. file name	
+    addr_len = sizeof their_addr;
+    if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0,
+        (struct sockaddr *)&their_addr, &addr_len)) == -1) {
+        perror("client: recvfrom error\n");
+        exit(1);
+    }
+
+	// 1.5 print file name
+	printf("*******************************\n");
+	buf[numbytes] = '\0';
+	filename = buf;
+	printf("client: got file name \"%s\" from %s\n", filename, 
+		inet_ntop(their_addr.ss_family,
+			get_in_addr((struct sockaddr *)&their_addr),
+			s, sizeof s));
+
+
+	// 2. file size
+    addr_len = sizeof their_addr;
+    if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0,
+        (struct sockaddr *)&their_addr, &addr_len)) == -1) {
+        perror("client: recvfrom error\n");
+        exit(1);
+    }
+
+	// 2.5 print file name
+	printf("*******************************\n");
+	buf[numbytes] = '\0';
+	filesize = atoi(buf);
+	printf("client: got file size \"%d\" from %s\n", filesize, 
+		inet_ntop(their_addr.ss_family,
+			get_in_addr((struct sockaddr *)&their_addr),
+			s, sizeof s));
+
+    
+    freeaddrinfo(servinfo);
+	outputFile = fopen(filename, "w");
+	if(outputFile == NULL)
+	{
+		printf("client: output file open error\n");
+		exit(1);
+	}
+	
+	//receive each frame and write it to the file stream at the offset 
+	//location that we can find from multiplying frame number (found in 
+	//the header) by the frame size
+	for(i = (filesize/FRAMESIZE); i >= 0; i--)
+	{
+		if ((numbytes = recvfrom(sockfd, &frH, sizeof(struct frameHeader), 0,
+        (struct sockaddr *)&their_addr, &addr_len)) == -1) {
+			perror("client: recvfrom error\n");
+			exit(1);
+		}
+
+		//move the file pointer and write to that location
+		fseek(outputFile, (frH.frameNum * FRAMESIZE), SEEK_SET);
+		fputs(frH.data, outputFile);
+	}
+	
+	//output the final file content for testing purposes
+	printf("*******************************\n");
+	outputFile = freopen(filename, "r", outputFile);
+	char temp[10000];
+	rewind(outputFile);
+	printf("File content:\n");
+	fgets(temp, 10000, outputFile);
+	puts(temp);
+	
+	// close socket
+    close(sockfd);
+	
+	//close file
+	fclose(outputFile);
+
+    return 0;
+}
+
---
+++ CS438-MP2/cs438-mp2-ntkach2-jsubida2/mp2server.c
@@ -0,0 +1,234 @@
+/*
+Beej's code modified as needed for mp2 cs438
+** listener.c -- a datagram sockets "server" demo
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <pthread.h>
+
+#include "mp2.h"
+
+#define MYPORT "1357"	// the port users will be connecting to
+
+#define MAXBUFLEN 10000
+#define FRAMESIZE 100
+
+
+struct frameHeader
+{
+	uint16_t checksum;
+	uint16_t frameNum;
+	uint8_t slt;
+	char data[FRAMESIZE];
+};
+
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+
+int main(int argc, char *argv[])
+{
+	int sockfd;
+	struct addrinfo hints, *servinfo, *p;
+	int rv;
+	int numbytes;
+	struct sockaddr_storage their_addr;
+	char buf[MAXBUFLEN];
+	size_t addr_len;
+	char s[INET6_ADDRSTRLEN];
+	int fsize;
+	char filesize[6];
+	FILE * inputFile;
+	struct frameHeader frH;
+	int i;
+	
+	memset(&frH, 0, sizeof(frH));
+
+	
+	//check if the name of the file present in the command line
+	if (argc < 2) {
+		fprintf(stderr,"usage: missing file name\n");
+		exit(1);
+	}
+	else if(argc > 2) {
+		fprintf(stderr,"usage: too many arguments\n");
+		exit(1);
+	}
+	
+	//open the file for reading
+	inputFile = fopen(argv[1], "r");
+	if(inputFile == NULL)
+	{
+		fprintf(stderr, "error: file does not exist");
+		exit(1);
+	}
+	
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC; // set to AF_INET to force IPv4
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_flags = AI_PASSIVE; // use my IP
+
+	if ((rv = getaddrinfo(NULL, MYPORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+	// loop through all the results and bind to the first we can
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+				p->ai_protocol)) == -1) {
+			perror("socket error");
+			continue;
+		}
+
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("bind error");
+			continue;
+		}
+
+		break;
+	}
+
+	if (p == NULL) {
+		fprintf(stderr, "bind socket error\n");
+		return 2;
+	}
+
+	freeaddrinfo(servinfo);
+
+	printf("server: waiting to recvfrom...\n");
+
+	
+	addr_len = sizeof their_addr;
+	if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0,
+			(struct sockaddr *)&their_addr, &addr_len)) == -1) {
+			perror("recvfrom error");
+			exit(1);
+	}
+
+	// print out data received
+    printf("server: got packet from %s\n",
+        inet_ntop(their_addr.ss_family,
+            get_in_addr((struct sockaddr *)&their_addr),
+            s, sizeof s));
+    printf("server: packet is %d bytes long\n", numbytes);
+    buf[numbytes] = '\0';
+    printf("server: packet contains \"%s\"\n", buf);
+		
+
+	
+	//send file name
+	if ((numbytes = mp2_sendto(sockfd, argv[1], strlen(argv[1]), 0,
+			 (struct sockaddr *) &their_addr, p->ai_addrlen)) == -1) {
+		perror("sendto error 1");
+		exit(1);
+	}
+	printf("server: sent %d bytes\n", numbytes);
+	printf("server: sent %s\n", argv[1]);
+	
+	
+	// determine and send file size
+    fseek(inputFile, 0, SEEK_END);
+    fsize = ftell(inputFile); 
+    fseek(inputFile, 0, SEEK_SET);
+    printf("File size: %d \n", fsize);
+	sprintf(filesize, "%d", (int) fsize);
+	
+	if ((numbytes = mp2_sendto(sockfd, filesize, strlen(filesize), 0,
+			 (struct sockaddr *) &their_addr, p->ai_addrlen)) == -1) {
+		perror("sendto error 2");
+		exit(1);
+	}
+	printf("server: sent %d bytes\n", numbytes);
+	printf("server: sent %s\n", filesize);
+	
+	
+	//initialize and create frames
+	frH.checksum = 0xaa;	//checksum initialized to 16 bit ones
+	frH.frameNum = 0x0;		//frame number initialized to zero
+	frH.slt = 0x04;		//the last byte initialized to sequence number zero, last frame mark set to one, and the frame type 3 bit zeros
+	uint8_t seq = 0x0;
+	uint8_t last = 0x1;
+	uint8_t type = 0x0;
+	uint16_t frame = 0x0;
+	
+	//iterate through the content of a file and send data in separate frames
+	for(i = fsize/FRAMESIZE; i > 0; i--)
+	{
+		fseek(inputFile, (frame * FRAMESIZE), SEEK_SET);
+		fgets(frH.data, FRAMESIZE+1, inputFile);
+		printf("----------------\n");
+		
+		if((fsize-(frame*FRAMESIZE)) >= FRAMESIZE)
+		{
+			last = 0x0;
+			frH.slt &= last;
+		}
+		
+		frH.slt = (seq << 4) | (last << 3) | (type);
+		frH.frameNum = frame;
+		
+		printf("frame: %d\n", frame);
+		printf("data: %s\n", frH.data);	
+		
+		if ((numbytes = mp2_sendto(sockfd, &frH, sizeof(frH), 0,
+			 (struct sockaddr *) &their_addr, p->ai_addrlen)) == -1) {
+			perror("server: sendto error 3\n");
+			exit(1);
+		}
+		
+		//wrap around sequence and frame numbers when they reach the limit
+		if(seq < 16)
+			seq = seq + 0x01;
+		else
+			seq = 0x0;
+			
+		if(frame < 65536)
+			frame = frame + 0x01;
+		else
+			frame = 0x0;
+	}
+	
+	//send the last frame for the file
+	last = 0x1;
+	frH.slt = (seq << 4) | (last << 3) | (type);
+	fseek(inputFile, (frame * FRAMESIZE), SEEK_SET);
+	fgets(frH.data, FRAMESIZE, inputFile);
+	frH.frameNum = frame;
+	printf("----------------\n");
+	printf("frame: %d\n", frame);
+	printf("data: %s\n", frH.data);
+	
+	if ((numbytes = mp2_sendto(sockfd, &frH, sizeof(frH), 0,
+			 (struct sockaddr *) &their_addr, p->ai_addrlen)) == -1) {
+		perror("server: sendto error 4\n");
+		exit(1);
+	}
+	
+	//close socket
+	close(sockfd);
+	
+	//close file
+	fclose(inputFile);
+
+	return 0;
+}
