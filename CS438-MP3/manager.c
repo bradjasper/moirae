b6fcfccce0f924a98150d845a8fa2ee866a48b01
JCadibuS <JCadibuS@8f81d046-9653-11dd-946f-4bcfd69f6e33>
2010-04-13 21:05:58 +0000

    adding Makefile and manager.c skeleton

--- CS438-MP3/manager.c
+++ CS438-MP3/manager.c
@@ -5,3 +5,28 @@
  * the madness.
  *
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+#include "parser.c"
+
+int main(int argc, char *argv[])
+{
+	// check number of arguments
+	if ( (argc < 3) || (argc > 3) )
+	{
+		fprintf(stderr, "usage: manager <file1.txt> <file2.txt> <file3.txt>");
+		exit(1);
+	}
+
+	// parse file 1, read network topology description
+
+	// fork each router, send connectivity information
+
+	return 0;
+
+}
