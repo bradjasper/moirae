db4cd42a8322895f27e99e1072417fef045bf443
nadia5188 <nadia5188@8f81d046-9653-11dd-946f-4bcfd69f6e33>
2010-04-19 03:26:52 +0000

    updated manager.c and new router.c

--- CS438-MP3/manager.c
+++ CS438-MP3/manager.c
@@ -1,8 +1,10 @@
 /** 
  * manager.c
  *
- * The Manager takes in three file names as arguments. It then handles 
- * the madness.
+ * Contains the "main" function for mp3 program
+ * Calls on parser.c to retrieve the number of nodes and network layout info
+ * Creates a new process for each router on the network and calls on router.c
+ * to execute the router functionality
  *
  */
 
@@ -11,22 +13,156 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
 
-#include "parser.c"
+#define PORT "1357"  // the port users will be connecting to
+
+#define BACKLOG 10	 // how many pending connections queue will hold
+
+void sigchld_handler(int s)
+{
+	while(waitpid(-1, NULL, WNOHANG) > 0);
+}
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+
 
 int main(int argc, char *argv[])
 {
-	// check number of arguments
-	if ( (argc < 3) || (argc > 3) )
-	{
-		fprintf(stderr, "usage: manager <file1.txt> <file2.txt> <file3.txt>");
+	//check for correct number of arguments received from command line
+	//there should be 4 arguments
+	if (argc < 4) {
+		fprintf(stderr,"usage: missing arguments\n");
+		exit(1);
+	}
+	else if(argc > 4) {
+		fprintf(stderr,"usage: too many arguments\n");
 		exit(1);
 	}
 
-	// parse file 1, read network topology description
+	
+	//parse the network structure information from the file
+	int numNodes = ; //call to parser.c
+	node * arrayNodes = ; //call to parser.c
+	
+	
+	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+	struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_storage their_addr; // connector's address information
+	socklen_t sin_size;
+	struct sigaction sa;
+	int yes = 1;
+	char s[INET6_ADDRSTRLEN];
+	int rv;
 
-	// fork each router, send connectivity information
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
 
+	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "manager: getaddrinfo error: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+	// loop through all the results and bind to the first we can
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+				p->ai_protocol)) == -1) {
+			perror("manager: socket");
+			continue;
+		}
+
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+				sizeof(int)) == -1) {
+			perror("manager: setsockopt");
+			exit(1);
+		}
+
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("manager: bind");
+			continue;
+		}
+		break;
+	}
+
+	if (p == NULL)  {
+		fprintf(stderr, "manager: failed to bind\n");
+		return 2;
+	}
+
+	freeaddrinfo(servinfo); // all done with this structure
+
+	if (listen(sockfd, BACKLOG) == -1) {
+		perror("manager: listen");
+		exit(1);
+	}
+
+	sa.sa_handler = sigchld_handler; // reap all dead processes
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+		perror("manager: sigaction");
+		exit(1);
+	}
+
+	printf("manager: waiting for connections...\n");
+	
+	//spawn a new process for each router
+	int i = 0;
+
+	while(1) {  // main accept() loop
+	
+		if(i < numNodes){
+			if (!fork()) 
+			{ // this is the child router process
+				executeRouter();
+			}
+			i++;
+		}
+	
+		sin_size = sizeof their_addr;
+		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+		if (new_fd == -1) {
+			perror("manager: accept");
+			continue;
+		}
+
+		inet_ntop(their_addr.ss_family,
+			get_in_addr((struct sockaddr *)&their_addr),
+			s, sizeof s);
+		printf("manager: got connection from %s\n", s);
+
+		if (!fork()) { // this is the child process
+//needs work here
+			close(sockfd); // child doesn't need the listener
+			if (send(new_fd, "Hello, world!", 13, 0) == -1)
+				perror("manager: send");
+			close(new_fd);
+			exit(0);
+		}
+		close(new_fd);  // parent doesn't need this
+	}
+	
+
+	
 	return 0;
-
 }
+
+
