067e4132891edf4c90c8b50a22abe115a043bb01
JCadibuS <JCadibuS@8f81d046-9653-11dd-946f-4bcfd69f6e33>
2010-04-20 06:31:32 +0000

    added util.c and node.c; parser is more or less done (need to iron out the c details)

---
+++ CS438-MP3/node.c
@@ -0,0 +1,85 @@
+/**
+ * node.c
+ *
+ * Variables and functions representing a node
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+struct node {
+	int node_num;
+	int arr_length;
+    int curr_index;
+	int *destin;
+    int *costs;
+    int *ports;
+} node;
+
+/**
+ *  PARAM: 	node - id of the source node to create
+ *			arr_l - length of the destin and costs arrays
+ *			cur_i - index of the destin and costs arrays
+ *			dest - id of the destination node
+ *			cost - cost from source to destin
+ * RETURN:  node *
+ *   FUNC:  initializes a node struct to the given params
+ */
+struct node *initNode(int node, int arr_l, int cur_i,
+						int dest, int cost)
+{
+
+	struct node *n = malloc(sizeof(node));
+
+	n->node_num = node;
+	n->arr_length = arr_l;
+	n->curr_index = cur_i;
+	
+	n->destin = malloc(arr_l * sizeof(int));
+	n->destin[0] = dest;	
+	
+	n->costs = malloc(arr_l * sizeof(int));
+	n->costs[0] = cost;
+	
+	int j;
+	for(j=0; j<arr_l; j++)
+	{
+		n->destin[j] = -1;
+		n->costs[j] = -1;
+	}
+
+	n->costs[0] = cost;
+	n->destin[0] = dest;	
+	
+	return n;
+
+}
+
+/**
+ *  PARAM:  *n - pointer to a node
+ * RETURN:  nothing
+ *   FUNC:	prints the node to stdout
+ */
+void print_node(struct node * n)
+{
+	printf("**************************\n");
+	if (n->node_num == -1)
+	{
+		printf("***EMPTY NODE***\n");
+		return;
+	}
+
+	printf("Node: %d\n", n->node_num);
+	printf("Destination Node, Cost\n");
+
+	int i;
+	for(i=0; i<n->arr_length; i++)
+	{
+		printf("%d, %d\n", n->destin[i], n->costs[i]);
+	}
+
+}
+
