5a7d6b592f800afdade280ed7037bc91f7222cc5
nadia5188 <nadia5188@8f81d046-9653-11dd-946f-4bcfd69f6e33>
2010-05-04 20:17:00 +0000

    just update, dv routing is under progress but is not linked to the execution processes yet

--- CS438-MP3/mp3.h
+++ CS438-MP3/mp3.h
@@ -17,6 +17,7 @@
 #include <sys/wait.h>
 #include <signal.h>
 
+
 // lsrouting.c
 typedef struct {
 	int neighor;
@@ -35,19 +36,40 @@ vector * initVector(int nearby, int price);
 
 void freeLsp(lsp * packet);
 
+
+//dvrouting.c
+typedef struct {
+	int numNodes;
+	int numNeighbors;
+	int * nextHop;
+	int * linkCost;
+} dvRoutingTable; //routing table at each node
+
+typedef struct {
+	int source;
+	int dest;
+	int seq_num;
+	int ttl;
+	int * path;	//path traversed
+	char * data;
+} dv; //packet format
+
+
+
 // parser.c
 int get_num_lines(FILE * file);
 
 struct node *parse(char * file);
 
+
 // node.c
 struct node {
-	int node_num;
-	int arr_length;
-    int curr_index;
-	int *destin;
-    int *costs;
-    int *ports;
+	int node_num;	//node ID
+	int arr_length;	//number of nodes on the network
+    int curr_index;	//number of neighbors 
+	int *destin;	//list of neighbors
+    int *costs;		//list of link costs
+    int *ports;		//list of port number for each neighbor
 } node;
 
 struct node *initNode(int node, int arr_l, int cur_i,
@@ -58,6 +80,7 @@ void addNode(struct node * topology, int src, int dest, int cost);
 void assignPorts(struct node * topology, int src, int port);
 
 void print_node(struct node * n);
+
 
 // router.c
 void sigchld_handler(int s);
