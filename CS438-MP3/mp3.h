2f0d4828322317b799186f18381e57fc7e1a37eb
nadia5188 <nadia5188@8f81d046-9653-11dd-946f-4bcfd69f6e33>
2010-05-05 01:35:05 +0000

    DV somewhat working but still not finished

--- CS438-MP3/mp3.h
+++ CS438-MP3/mp3.h
@@ -17,6 +17,9 @@
 #include <sys/wait.h>
 #include <signal.h>
 
+#include <stdio_ext.h>
+
+
 
 // lsrouting.c
 typedef struct {
@@ -35,25 +38,6 @@ lsp * initLsp(int maker, int sequence, int lifetime);
 vector * initVector(int nearby, int price);
 
 void freeLsp(lsp * packet);
-
-
-//dvrouting.c
-typedef struct {
-	int numNodes;
-	int numNeighbors;
-	int * nextHop;
-	int * linkCost;
-} dvRoutingTable; //routing table at each node
-
-typedef struct {
-	int source;
-	int dest;
-	int seq_num;
-	int ttl;
-	int * path;	//path traversed
-	char * data;
-} dv; //packet format
-
 
 
 // parser.c
@@ -88,3 +72,34 @@ void sigchld_handler(int s);
 void *get_in_addr(struct sockaddr *sa);
 
 int executeRouter();
+
+
+//dvrouting.c
+typedef struct {
+	int nodeID;
+	int numNodes;
+	int numNeighbors;
+	int * nextHop;
+	int * linkCost;
+} dvRoutingTable; //routing table at each node
+
+typedef struct {
+	int type;		//type of message: 1 for network update, 2 for data
+	int source;
+	int dest;		//-1 for network update, nodeID for data packet destination
+	int seq_num;
+	int ttl;
+	int * path;		//path traversed
+	void * data;	//dvRoutingTable* for network update, char* for data
+} dv; //packet format
+
+void printRoutingTable(dvRoutingTable * table);
+
+dvRoutingTable * initDVroutingTable(struct node * connectTable);
+
+dv * initDVpacket(struct node * connectTable, dvRoutingTable * table, int type, int des, int seq);
+
+void updateExchange(dvRoutingTable * table, struct node * connectTable, char * hostname);
+
+void dvRouting(struct node * connectTable);
+
