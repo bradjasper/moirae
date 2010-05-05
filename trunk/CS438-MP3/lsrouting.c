252aca07c514f1470943eedf66dd3aa0e63e57b6
JCadibuS <JCadibuS@8f81d046-9653-11dd-946f-4bcfd69f6e33>
2010-05-05 22:32:43 +0000

    major changes to mp3.h and lsrouting.c

--- trunk/CS438-MP3/lsrouting.c
+++ trunk/CS438-MP3/lsrouting.c
@@ -6,6 +6,24 @@
 
  #include "mp3.h"
 
+
+/**
+ *  PARAM:  packet - lsp for source node
+ * RETURN:  lsRoutingTable *
+ *   FUNC:	initializes an lsRoutingTable
+ */
+lsRoutingTable * initLsRoutingTable(lsp * packet)
+{
+	lsRoutingTable *lsRT = (lsRoutingTable*)malloc(sizeof(lsRoutingTable) + sizeof(lsp));
+
+	lsRT->source_id = packet->creator_id;
+	lsRT->num_packets = 1;
+	lsRT->known_packets = (lsp*)malloc(sizeof(lsp));
+	lsRT->known_packets[0] = *packet;
+
+	return lsRT;
+}
+
 /**
  *  PARAM:  maker - source node to init to
  *			sequence - number of hops to init to
@@ -15,7 +33,7 @@
  */
 lsp * initLsp(struct node * maker, int sequence, int lifetime)
 {
-	int num_neighbors = maker->curr_index;
+	int num_neighbors = maker->arr_length;
 	int *neighbors = maker->destin;
 	int *costs = maker->costs;
 	int *ports = maker->ports;
@@ -79,19 +97,66 @@ void freeLsp(lsp * packet)
  * RETURN:  
  *   FUNC:	
  */
+void freeLsRoutingTable(lsRoutingTable * lsrt)
+{
+	int num_packs = lsrt->num_packets;
+	lsp * packets = lsrt->known_packets;
+	
+	int i;
+	for (i=0; i<num_packs; i++)
+		freeLsp(&packets[i]);
+	
+	free(lsrt);
+}
+
+/**
+ *  PARAM:  
+ * RETURN:  
+ *   FUNC:	
+ */
 void lsRouting(struct node * a_node)
 {
-	char * hostname;
-	int len;
+	char hostname[32];
+	int len = 32;
 
 	if (gethostname(hostname, len) != 0)
 	{
 		fprintf(stderr, "lsrouting: gethostname error\n");
 		return;
 	}
-
-
+	
+	lsp * myLsp = initLsp(a_node, -1, -1);
+	lsRoutingTable * myLsrt = initLsRoutingTable(myLsp);
+	print_ls_table(myLsrt);
 }
 
+/**
+ *  PARAM:  
+ * RETURN:  
+ *   FUNC:	
+ */
+void print_ls_table(lsRoutingTable * lsrt)
+{
+	printf("*******************************\n");
+	printf("Table for node: %d\n", lsrt->source_id);
+	printf("destination\tneighbor\tcost\n");
+	
+	int i,j;
+	for (i=0; i<lsrt->num_packets; i++)
+	{
+		lsp * cur_lsp = &(lsrt->known_packets[i]);
+		
+		int destin = cur_lsp->creator_id;
 
+		int num_neighbors = cur_lsp->num_neighbors;
+		vector * neighbors = cur_lsp->neighbors;
+		for (j=0; j<num_neighbors; j++)
+		{
+			vector * vec = &neighbors[j];
+			int neighbor = vec->neighbor;
+			int cost = vec->cost;
+			printf("%d\t%d\t%d\n", destin, neighbor, cost);
+		}
+	}
+}
 
